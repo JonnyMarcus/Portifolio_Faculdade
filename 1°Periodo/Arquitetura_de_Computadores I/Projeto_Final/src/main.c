396e68124dd2919066dfa0cd4e4dbb61e712a84f
JonnyMarcus <jonnymarcusmilitao123@gmail.com>
2026-05-03 15:16:32 -0300

    estrutura do projeto e menu

---
+++ 1°Periodo/Arquitetura_de_Computadores I/Projeto_Final/src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void menu() {
+    printf("\n=== SIMULADOR DE ARQUITETURA ===\n");
+    printf("1. Conversão de números\n");
+    printf("2. Operações binárias\n");
+    printf("3. Simulador de memória\n");
+    printf("4. Calculo de bits/bytes\n");
+    printf("0. Sair\n");
+}
+int main(){
+    
+
+
+    return 0;
+    
+}
