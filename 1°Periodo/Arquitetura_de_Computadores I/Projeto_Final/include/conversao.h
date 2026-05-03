c55e64396e9a56b3a53f22151178d95a8a2a50a7
JonnyMarcus <jonnymarcusmilitao123@gmail.com>
2026-05-03 16:51:58 -0300

    .h conversao

---
+++ 1°Periodo/Arquitetura_de_Computadores I/Projeto_Final/include/conversao.h
@@ -0,0 +1,13 @@
+#ifndef CONVERSAO_H
+
+#define CONVERSAO_H
+
+int binario_decimal( int bin );
+int decimal_binario(int num);
+int binario_hexadecimal(int bin );
+int hexadecimal_binario(int bin );
+int binario_octal(int bin );
+int octal_binario(int oct);
+
+
+#endif CONVERSAO_H
---
+++ 1°Periodo/Arquitetura_de_Computadores I/Projeto_Final/src/conversao.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "conversao.h"
+
+
+
+int main(){
+
+
+
+
+
+
+
+
+
+    return 0;
+}
