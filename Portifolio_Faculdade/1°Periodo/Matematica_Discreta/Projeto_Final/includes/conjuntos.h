73e841347487f58048c986348d99818f8696bc71
JonnyMarcus <jonnymarcusmilitao123@gmail.com>
2026-06-20 19:09:49 -0300

    fix: arrumando arquivo.h do conjunto

--- Portifolio_Faculdade/1°Periodo/Matematica_Discreta/Projeto_Final/includes/conjuntos.h
+++ Portifolio_Faculdade/1°Periodo/Matematica_Discreta/Projeto_Final/includes/conjuntos.h
@@ -3,8 +3,8 @@
 
 #include "utils.h"
 
-void uniao(int A[], int nA[], int B[], int nB[]);
-void intersecao(int A[], int nA[], int B[], int nB[]);
+void uniao(int A[], int nA, int B[], int nB);
+void intersecao(int A[], int nA, int B[], int nB);
 void diferenca(int A[], int nA, int B[], int nB);
 void diferenca_simetrica(int A[], int nA, int B[], int nB);
 void pertinencia(int A[], int nA, int x);
