f6780a7a10aa71925b1f3c9e94a5f5c5401149ca
JonnyMarcus <jonnymarcusmilitao123@gmail.com>
2026-06-20 15:53:10 -0300

    fix: resolve o arquirvo .h de relaçoes

--- Portifolio_Faculdade/1°Periodo/Matematica_Discreta/Projeto_Final/includes/relacoes.h
+++ Portifolio_Faculdade/1°Periodo/Matematica_Discreta/Projeto_Final/includes/relacoes.h
@@ -1,10 +1,12 @@
 #ifndef RELACOES_H
 #define RELACOES_H
 
+#include "utils.h"
+
 void produto_cartesiano(int A[], int nA, int B[], int nB);
-void verifica_reflexivo(int A[][], int nA);
-void verifica_simetrica(int A[][], int nA);
-void verifica_transitiva(int A[][], int nA);
+void verifica_reflexiva(int A[][MAX_ELEMENTOS], int nA);
+void verifica_simetrica(int A[][MAX_ELEMENTOS], int nA);
+void verifica_transitiva(int A[][MAX_ELEMENTOS], int nA);
 void menu_relacoes();
 
 #endif
