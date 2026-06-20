0781c51c1db5b2800bfacf053ca3557d88c9ed79
JonnyMarcus <jonnymarcusmilitao123@gmail.com>
2026-06-20 19:30:43 -0300

    feat: add makefile e corrige arquivo.c da conversao

--- Portifolio_Faculdade/1°Periodo/Arquitetura_de_Computadores I/Projeto_Final/src/conversao.c
+++ Portifolio_Faculdade/1°Periodo/Arquitetura_de_Computadores I/Projeto_Final/src/conversao.c
@@ -1,91 +1,87 @@
 #include <stdio.h>
 #include <string.h>
-
 #include "conversao.h"
 
-void binario_decimal(){
-    int decimal =0;
+void binario_decimal()
+{
+    int decimal = 0;
     char bin[50];
-    
-    printf("escreva o numero binario: ");
-    scanf("%s",&bin);
 
-    for(int i =0 ;bin[i] != "\0";i++){
+    printf("escreva o numero binario: ");
+    scanf("%s", bin);
+
+    for (int i = 0; bin[i] != '\0'; i++)
+    {
         int bit = bin[i] - '0';
         decimal = decimal * 2 + bit;
     }
     printf("\nDecimal: %d\n", decimal);
-
 }
 
-void decimal_binario(){
+void decimal_binario()
+{
     char bin[50];
     int dec;
-    int i =0;
+    int i = 0;
 
-    printf("escreva o decimal:  ");
-    scanf("%d",&dec);
+    printf("escreva o decimal: ");
+    scanf("%d", &dec);
 
-    if (dec ==0){
+    if (dec == 0)
+    {
         printf("0\n");
-        return; 
+        return;
+    }
+
+    while (dec > 0)
+    {
+        bin[i++] = dec % 2;
+        dec = dec / 2;
     }
 
     printf("\nBinario: ");
-
-    for(int j = i - 1; j >= 0; j--) {
+    for (int j = i - 1; j >= 0; j--)
+    {
         printf("%d", bin[j]);
     }
-
     printf("\n");
 }
-void binario_hexadecimal(){
+
+void binario_hexadecimal()
+{
     char bin[50];
     char temp[50];
+    temp[0] = '\0';
 
     printf("digite o numero binario: ");
-    scanf("%s",bin);
+    scanf("%s", bin);
 
     int len = strlen(bin);
-    int resto = len %4;
-    if(resto !=0){
+    int resto = len % 4;
+    if (resto != 0)
+    {
         int faltam = 4 - resto;
-        for(int i = 0; i < faltam; i++) {
+        for (int i = 0; i < faltam; i++)
             strcat(temp, "0");
-        }
         strcat(temp, bin);
         strcpy(bin, temp);
     }
 
     printf("Hexadecimal: ");
-    for(int i = 0; bin[i] != '\0'; i += 4) {
+    for (int i = 0; bin[i] != '\0'; i += 4)
+    {
         int valor = 0;
-
-        for(int j = 0; j < 4; j++) {
+        for (int j = 0; j < 4; j++)
             valor = valor * 2 + (bin[i + j] - '0');
-        }
 
-        
-        if(valor < 10) {
+        if (valor < 10)
             printf("%d", valor);
-        } else {
+        else
             printf("%c", valor - 10 + 'A');
-        }
     }
-
     printf("\n");
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-   
+void menu_conversao()
+{
+}
