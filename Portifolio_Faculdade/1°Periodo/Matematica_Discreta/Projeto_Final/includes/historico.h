4543418e41cc43e9ceeef8d91b8028e23a9f5749
JonnyMarcus <jonnymarcusmilitao123@gmail.com>
2026-06-20 18:31:12 -0300

    feat: adiciona arquivo .h das funçoes e suas assinaturas

--- Portifolio_Faculdade/1°Periodo/Matematica_Discreta/Projeto_Final/includes/historico.h
+++ Portifolio_Faculdade/1°Periodo/Matematica_Discreta/Projeto_Final/includes/historico.h
@@ -1,4 +1,9 @@
-#ifndef hisstorico.h
-#define historico .h
+#ifndef HISTORICO_H
+#define HISTORICO_H
+
+void salvar_historico(char *mensagem);
+void consultar_historico();
+void limpar_historico();
+void menu_historico();
 
 #endif
