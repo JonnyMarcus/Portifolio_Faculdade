6bc86f4085a0a3921d17b99d0683923b158c7cf8
JonnyMarcus <jonnymarcusmilitao123@gmail.com>
2026-06-20 03:08:14 -0300

    feat: add arquivo .h logica booleana

--- Portifolio_Faculdade/1°Periodo/Matematica_Discreta/Projeto_Final/includes/logica.h
+++ Portifolio_Faculdade/1°Periodo/Matematica_Discreta/Projeto_Final/includes/logica.h
@@ -1,4 +1,15 @@
-#ifndef logica.h
-#define logica .h
+#ifndef LOGICA_H
+#define LOGICA_H
+
+#include "utils.h"
+
+int op_and(int p, int q);
+int op_or(int p, int q);
+int op_not(int p);
+int op_xor(int p, int q);
+int op_implicacao(int p, int q);
+int op_bicondicional(int p, int q);
+void tabela_verdade();
+void menu_logica();
 
 #endif
