33d30057eb2bd7b7cf5bfdbe2a09b7c3f431a1ac
JonnyMarcus <jonnymarcusmilitao123@gmail.com>
2026-06-20 16:39:04 -0300

    feat: adiciona funçao .h de grafo

--- Portifolio_Faculdade/1°Periodo/Matematica_Discreta/Projeto_Final/includes/grafo.h
+++ Portifolio_Faculdade/1°Periodo/Matematica_Discreta/Projeto_Final/includes/grafo.h
@@ -1,4 +1,26 @@
-#ifdef grafo.h
-#define grafo .h
+#ifndef GRAFO_H
+#define GRAFO_H
+#include "utils.h"
+
+typedef struct
+{
+    int adj[MAX_ELEMENTOS][MAX_ELEMENTOS];
+    int nVertices;
+
+} grafo;
+
+void cria_grafo(grafo *g);
+
+void inseri_vertice(grafo *g);
+
+void inseri_aresta(grafo *g);
+
+void grau_vertice(grafo *g);
+
+void BFS(grafo *g);
+
+void DFS(grafo *g);
+
+void menu_grafo(grafo *g);
 
 #endif
