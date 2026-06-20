0f5b7fc8b48a0be2be3148ce8aac795d754767b0
JonnyMarcus <jonnymarcusmilitao123@gmail.com>
2026-06-20 16:12:43 -0300

    feat: adiciona funçao para verificar matriz transitiva

--- Portifolio_Faculdade/1°Periodo/Matematica_Discreta/Projeto_Final/src/relacoes.c
+++ Portifolio_Faculdade/1°Periodo/Matematica_Discreta/Projeto_Final/src/relacoes.c
@@ -48,3 +48,25 @@ void verifica_simetrica(int A[][100], int nA)
     else
         printf("A relacao NAO E simetrica\n");
 }
+void verifica_transitiva(int A[][MAX_ELEMENTOS], int nA)
+{
+    int transitiva = 1;
+
+    for (int i = 0; i < nA; i++)
+    {
+        for (int j = 0; j < nA; j++)
+        {
+            for (int k = 0; k < nA; k++)
+            {
+
+                if (A[i][j] == 1 && A[j][k] == 1 && A[i][k] == 0)
+                    transitiva = 0;
+            }
+        }
+    }
+
+    if (transitiva == 1)
+        printf("A relacao transitivo \n");
+    else
+        printf("A relacao NAO Etransitivo transitivo\n");
+}
